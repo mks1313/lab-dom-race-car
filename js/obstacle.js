d337acc53626cdee97555ba2f18aee87ccfc3c07
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-01-11 19:30:52 +0100

    done

---
+++ js/obstacle.js
@@ -0,0 +1,17 @@
+class Obstacle extends Component {
+    constructor(gameScreen) {
+      super(
+        gameScreen,
+        Math.floor(Math.random() * 300 + 70),
+        0,
+        100,
+        150,
+        "./images/redCar.png"
+      );
+    }
+  
+    move() {
+      this.top += 3;
+      this.updatePosition();
+    }
+  }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,58 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
 
   startButton.addEventListener("click", function () {
     startGame();
   });
 
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  });
+
   function startGame() {
     console.log("start game");
+    game = new Game();
+
+    game.start();
   }
+
+  function restartGame() {
+    location.reload();
+  }
+
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+      switch (key) {
+        case "ArrowLeft":
+          console.log("pressed LEFT");
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          console.log("pressed UP");
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          console.log("pressed RIGHT");
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          console.log("pressed DOWN");
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  window.addEventListener("keydown", handleKeydown);
 };
